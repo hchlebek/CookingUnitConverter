d0826612661399ade799a1a3aafdc28b8675275e
HChlebek <student@hchlebek>
2015-12-19 20:15:39 -0600

    Initial Commit

---
+++ Cooking Unit Converter FInalTests/Cooking_Unit_Converter_FInalTests.swift
@@ -0,0 +1,36 @@
+//
+//  Cooking_Unit_Converter_FInalTests.swift
+//  Cooking Unit Converter FInalTests
+//
+//  Created by HChlebek on 12/19/15.
+//  Copyright © 2015 HChlebek. All rights reserved.
+//
+
+import XCTest
+@testable import Cooking_Unit_Converter_FInal
+
+class Cooking_Unit_Converter_FInalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
